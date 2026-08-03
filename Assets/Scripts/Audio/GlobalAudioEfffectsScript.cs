16fe21b687031f096f20351a45e240681e80637b
saumitrm13 <mulaysaumitr@gmail.com>
2026-08-03 22:07:33 +0530

    Button click sfx
    Button click sfx hooked to every existing button in the hierarchy at the time of game start

---
+++ Assets/Scripts/Audio/GlobalAudioEfffectsScript.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalAudioEfffectsScript : MonoBehaviour
+{
+    public static GlobalAudioEfffectsScript Instance { get; private set; }
+
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip clickClip;
+
+    private void Awake()
+    {
+        if (Instance != null) { Destroy(gameObject); return; }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void OnEnable()
+    {
+        // Hook every button currently in the scene
+        HookAllButtons();
+    }
+
+    public void HookAllButtons()
+    {
+        foreach (var btn in FindObjectsByType<Button>(FindObjectsInactive.Include, FindObjectsSortMode.None))
+        {
+            btn.onClick.RemoveListener(PlayClickSound); // avoid double-hook
+            btn.onClick.AddListener(PlayClickSound);
+        }
+    }
+
+    public void PlayClickSound()
+    {
+        if (clickClip != null)
+            audioSource.PlayOneShot(clickClip);
+    }
+}
